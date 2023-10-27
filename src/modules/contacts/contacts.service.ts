53ef6c010aaef6ed6d9fe43748c1716b25af8b3b
VoDinhNghia <vodinhnghia85@gmail.com>
2023-10-27 19:57:22 +0700

    [contact module] create entity and dtos file

--- src/modules/contacts/contacts.service.ts
+++ src/modules/contacts/contacts.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contact } from './entities/contacts.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ContactsService {}
+export class ContactsService {
+  constructor(
+    @InjectRepository(Contact)
+    private contactRepo: Repository<Contact>,
+  ) {}
+}
