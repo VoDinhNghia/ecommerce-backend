9fde9a36be84dc361af196fc892e3f8c7a99844f
VoDinhNghia <vodinhnghia85@gmail.com>
2023-10-28 19:50:46 +0700

    [questions module] create entity file

--- src/modules/questions/questions.module.ts
+++ src/modules/questions/questions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Questions } from './entities/questions.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Questions])],
   controllers: [QuestionsController],
   providers: [QuestionsService],
 })
