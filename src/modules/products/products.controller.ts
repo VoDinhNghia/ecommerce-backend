10936278f5b5fe5751d13402e9b73db6d6585ace
VoDinhNghia <vodinhnghia85@gmail.com>
2023-11-09 19:30:55 +0700

    [products module] write api discount

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -23,6 +23,9 @@ import { productMsg } from 'src/constants/constants.message.response';
 import { UpdateProductDto } from './dtos/products.update.dto';
 import { QueryProductDto } from './dtos/products.query.dto';
 import { CreateProductDetailDto } from './dtos/products.create-detail.dto';
+import { CreateProductDiscountDto } from './dtos/products.create-discount.dto';
+import { UpdateProductDiscount } from './dtos/products.update-discount.dto';
+import { UpdateProductDetailDto } from './dtos/products.update-detail.dto';
 
 @Controller(productController.name)
 @ApiTags(productController.tag)
@@ -93,10 +96,47 @@ export class ProductsController {
   @UseGuards(RoleGuard([ErolesUser.SUPPER_ADMIN]))
   async updateDetail(
     @Param('id') id: string,
-    @Body() detailDto: CreateProductDetailDto,
+    @Body() detailDto: UpdateProductDetailDto,
     @Res() res: Response,
   ): Promise<ResponseRequest> {
     await this.service.updateDetail(id, detailDto);
     return new ResponseRequest(res, true, productMsg.updateDetail);
+  }
+
+  @Post('/discount')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RoleGuard([ErolesUser.SUPPER_ADMIN]))
+  async createDiscount(
+    @Body() discountDto: CreateProductDiscountDto,
+    @Res() res: Response,
+  ): Promise<ResponseRequest> {
+    const result = await this.service.createDiscount(discountDto);
+    return new ResponseRequest(res, result, productMsg.createDiscount);
+  }
+
+  @Put('/discount/:id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RoleGuard([ErolesUser.SUPPER_ADMIN]))
+  async updateDiscount(
+    @Param('id') id: string,
+    @Body() discountDto: UpdateProductDiscount,
+    @Res() res: Response,
+  ): Promise<ResponseRequest> {
+    await this.service.updateDiscount(id, discountDto);
+    return new ResponseRequest(res, true, productMsg.updateDiscount);
+  }
+
+  @Delete('/discount/:id')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RoleGuard([ErolesUser.SUPPER_ADMIN]))
+  async deleteDiscount(
+    @Param('id') id: string,
+    @Res() res: Response,
+  ): Promise<ResponseRequest> {
+    await this.service.deleteDiscount(id);
+    return new ResponseRequest(res, true, productMsg.deleteDiscount);
   }
 }
